9d8ae7b1167d8ea88e3f692a131b5f497dcadf96
JoaoPiero <piro.garcia@hotmail.com>
2019-03-09 00:34:11 -0300

    Implementação do metodo rolarAtributos

--- src/projetoRPG/Personagem.java
+++ src/projetoRPG/Personagem.java
@@ -1,12 +1,14 @@
 package projetoRPG;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
-@Entity
+// @Entity
 
 public class Personagem {
 	
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+//	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id; 
 	private String nomePersonagem ;
 	private Classe classe ;
@@ -16,24 +18,25 @@ public class Personagem {
 	private Integer experiencia;
 	private Status status;
 	private Equipamento equipamento;
-	
-	
-	int rolarDado(int tipodado) {
 		
+	int rolarDado(int tipodado) {
+		int resultado = 0 ;
 		if (tipodado <= 1) {
-			System.out.println("Dado invalidos");
+			System.out.println("Dado invalido");
 			
 		}else {
 			Random random = new Random ();
-			int resultado = random.nextInt(tipodado) + 1;
+			resultado = random.nextInt(tipodado) + 1;
 			System.out.printf("%d, ",resultado);	
-			return resultado ;
+			
 		}
-		
+		return resultado ;
 	}
 	
 	int rolarDadosComBonus(int quantidadeDados, int tipoDado, int bonusDado) {
+		
 		int soma = 0;
+		
 		if (quantidadeDados <= 0 || tipoDado <=0) {
 			System.out.println("Dado invalido.");
 			
@@ -50,8 +53,40 @@ public class Personagem {
 		return soma;
 	}
 	
-	int rolarAtributos() {
+	ArrayList rolarAtributos() {
 		
+		ArrayList<Integer> resultadoOrdenado = new ArrayList<Integer>();
+		
+		for (int atributo = 1; atributo <= 6; atributo++) {
+		
+			ArrayList<Integer> resultadoAtributo = new ArrayList<Integer>();
+			
+			for (int dadoDeAtributo = 1; dadoDeAtributo <= 4; dadoDeAtributo++) {
+
+				resultadoAtributo.add(rolarDado(6)) ;
+					
+			}
+			
+			Collections.sort(resultadoAtributo);
+            
+			resultadoAtributo.remove(0);
+			
+			int soma = 0 ;
+			for (Integer i : resultadoAtributo) {
+				
+				soma += i ;
+			}
+			
+			if (soma < 8) {
+				soma = 8 ;
+			}
+			
+			resultadoOrdenado.add(soma) ;
+			
+		}
+		Collections.sort(resultadoOrdenado);
+        Collections.reverse(resultadoOrdenado);
+		return resultadoOrdenado;
 	}
 
 }
